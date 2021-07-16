1fdeb3c681de5cbb6d781a0a56d6025bb59269f2
jjb0093 <jjb0093@naver.com>
2021-07-17 01:20:42 +0900

    Add files via upload
    지원서 작성 및 신청 관련 파일입니다.

---
+++ sourceCode/write.php
@@ -0,0 +1,152 @@
+<?php
+  $clubID = $_GET['id'];
+
+  include $_SERVER['DOCUMENT_ROOT'].'/databaseConnection_bongClub.php';
+
+  $sql = "SELECT * FROM clubinfo WHERE clubID = {$clubID}";
+  $result = mysqli_query($dbConnect,$sql);
+  $row = mysqli_fetch_array($result);
+
+  $sql_f ="SELECT * FROM clubques WHERE clubID = {$clubID}";
+  $result_f = mysqli_query($dbConnect,$sql_f);
+  $row_f = mysqli_fetch_array($result_f);
+  $count_f = mysqli_num_rows($result_f);
+?>
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+  <title>지원서 작성 (<?echo $row['clubName'];?>)</title>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Gamja+Flower&family=Nanum+Gothic&family=Noto+Sans+KR&family=Sunflower:wght@300&display=swap" rel="stylesheet">
+  <style>
+  /*
+  font-family: 'Do Hyeon', sans-serif;
+  font-family: 'Gamja Flower', cursive;
+  font-family: 'Nanum Gothic', sans-serif;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-family: 'Sunflower', sans-serif;
+  */
+    html, body{
+      height: 100%;
+      width: 100%;
+    }
+    body{
+      margin: 0 0 0 0;
+    }
+    #header{
+      height: 50px;
+      position: relative;
+      background-color: rgba(137,213,250,0.6);
+    }
+    #header span{
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      font-family: 'Noto Sans KR', sans-serif;
+      font-size: 25px;
+    }
+    form{
+      width: 100%;
+      height: auto;
+    }
+    form #stuInfo{
+      width: 100%;
+      padding: 2% 0 2% 0;
+      text-align: center;
+    }
+    form #stuInfo label{
+      font-family: 'Noto Sans KR', sans-serif;
+      font-size: 20px;
+    }
+    form #stuInfo select{
+      font-family: 'Noto Sans KR', sans-serif;
+      font-size: 20px;
+    }
+    form #stuInfo input[type="text"]{
+      font-family: 'Noto Sans KR', sans-serif;
+      font-size: 20px;
+    }
+    form #stuInfo input[type="text"][name="name"]{
+      width: 130px;
+    }
+    form #stuInfo input[type="text"][name="password"]{
+      width: 200px;
+    }
+    #form{
+      width: 100%;
+      padding: 2% 0 2% 0;
+      height: auto;
+      background-color: rgba(255,227,0,0.2);
+    }
+    #form .title{
+      display: inline-block;
+      margin-left: 3%;
+      vertical-align: top;
+      font-family: 'Noto Sans KR', sans-serif;
+      font-size: 22px;
+      font-weight: bold;
+    }
+    #form textarea{
+      font-family: 'Noto Sans KR', sans-serif;
+      font-size: 18px;
+      display: inline-block;
+      margin-left: 3%;
+      margin-top: 1%;
+      width: 94%;
+    }
+    #form input[type="submit"]{
+      display: inline-block;
+      width: 94%;
+      padding: 2% 0 2% 0;
+      border-radius: 20px;
+      margin-top: 2%;
+      font-family: 'Noto Sans KR', sans-serif;
+      font-size: 20px;
+      cursor: pointer;
+      transition: all 1s;
+    }
+    #form input[type="submit"]:hover{
+      background-color: rgba(217,239,255);
+    }
+  </style>
+</head>
+<body>
+  <div id="header">
+    <span>지원서 작성 (<?echo $row['clubName'];?>)</span>
+  </div>
+  <form method="post" action="write_Check?id=<?echo $clubID;?>">
+    <div id="stuInfo">
+      <label for="grade">학년</label>
+        <select>
+          <option value="어차피 수정 불가능하지롱">1학년</option>
+        </select>&nbsp;&nbsp;
+      <label for="class">반</label>
+        <select name="class" required>
+          <option disabled selected value> 반 선택 </option>
+          <? for($i=1;$i<=9;$i++){ ?>
+          <option value="<?echo $i;?>"><?echo $i;?>반</option>
+          <? } ?>
+        </select>&nbsp;&nbsp;
+      <label for="class">번호</label>
+        <select name="num" required>
+          <option disabled selected value> 번호 선택 </option>
+          <? for($i=1;$i<=32;$i++){ ?>
+          <option value="<?echo $i;?>"><?echo $i;?>번</option>
+          <? } ?>
+        </select>&nbsp;&nbsp;
+      <label for="name">이름</label>
+        <input type="text" name="name" id="name" placeholder="이름" required>&nbsp;&nbsp;
+      <label for="password">비밀번호</label>
+        <input type="text" name="password" id="password" placeholder="비밀번호(수정용)" required>
+    </div>
+    <div id="form">
+      <? for($i=1;$i<=6;$i++){ if($row_f['ques_'.$i]==null) break; ?>
+        <span class="title"><? echo $i.') '.$row_f['ques_'.$i]; ?></span><br>
+        <textarea required placeholder="항상 신중하고, 진지하게. 한 번 선택한 동아리가 1년의 생기부를 결정합니다." name="ans_<?echo $i?>" rows="5"></textarea><br><br><br>
+      <? } ?>
+      <center><input type="submit" value="제출" onclick="return confirm('등록하시겠습니까?')"></center>
+    </div>
+  </form>
+</body>
+</html>
